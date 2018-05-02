0cdf08a604f1ed32dd921c72bfe2421217c884a0
trickstival <trick_stival@hotmail.com>
2018-05-01 22:46:32 -0300

    Query and method extracted

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-  res.end('Hello world!')
+
+  const parsedUrl = url.parse(req.url, true)
+  const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
+
+  const method = req.method.toLowerCase()
+
+  const query = parsedUrl.query
+
+  console.log(`Going to respond with query`, query)
+  
+  res.end(`hit ${trimmedPath} by ${method} method\n`)
+})
+
+server.listen(3001, () => {
+  console.log('Server initialized!')
 })
